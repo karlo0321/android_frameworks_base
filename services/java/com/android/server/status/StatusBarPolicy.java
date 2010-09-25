08da7253c1fee310b95d983b161861d68b0e90ee
Eran <eranmizra@gmail.com>
2010-09-25 06:07:36 +0200

    Fix previous commit (449592cef)
    Change-Id: I56352a663f712a861e6ee7a0985c746e17f0450c

--- services/java/com/android/server/status/StatusBarPolicy.java
+++ services/java/com/android/server/status/StatusBarPolicy.java
@@ -443,7 +443,7 @@ public class StatusBarPolicy {
                 null, com.android.internal.R.drawable.stat_sys_battery_unknown, 0, 0,
                 Settings.System.BATTERY_PERCENTAGE_STATUS_COLOR);
 	mBatteryData.textColor = Settings.System.getInt(mContext.getContentResolver(),
-							Settings.System.BATTERY_PERCENTAGE_STATUS_COLOR, blackColor);
+							Settings.System.BATTERY_PERCENTAGE_STATUS_COLOR, 0xff000000);
         mBatteryIcon = service.addIcon(mBatteryData, null);
 
         // phone_signal
