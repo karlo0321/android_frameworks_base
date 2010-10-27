d4fa7e91f38059b5fbd4aca78b9b76596657f612
Eran <eranmizra@gmail.com>
2010-10-27 18:28:14 +0200

    Disable mobile data by default
    Change-Id: I06458a9fc074a077d67c46ac5117d82d88ec7ca5

--- services/java/com/android/server/ConnectivityService.java
+++ services/java/com/android/server/ConnectivityService.java
@@ -794,7 +794,7 @@ public class ConnectivityService extends IConnectivityManager.Stub {
     public boolean getMobileDataEnabled() {
         enforceAccessPermission();
         boolean retVal = Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.MOBILE_DATA, 1) == 1;
+                Settings.Secure.MOBILE_DATA, 0) == 1;
         if (DBG) Slog.d(TAG, "getMobileDataEnabled returning " + retVal);
         return retVal;
     }
