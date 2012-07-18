96957d2b91fa70170f81a76bf7bfeaa432ee47cc
nobodyAtall <tsogkas@ceid.upatras.gr>
2012-07-18 16:50:56 +0300

    Delay bootanimation until the kernel boot animation is done.
    Change-Id: Ib40d95445ad7c2a9132d1899aeee99bbbf37d50e

--- cmds/bootanimation/bootanimation_main.cpp
+++ cmds/bootanimation/bootanimation_main.cpp
@@ -47,6 +47,10 @@ int main(int argc, char** argv)
     int noBootAnimation = atoi(value);
     ALOGI_IF(noBootAnimation,  "boot animation disabled");
     if (!noBootAnimation) {
+        while (0 == system("pidof bootlogo > /dev/null")) {
+            //LOGE("boologo is still running...");
+            sleep(1);
+        }
 
         sp<ProcessState> proc(ProcessState::self());
         ProcessState::self()->startThreadPool();
